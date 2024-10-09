f30b00b1136ec1cc07c1b0683630c348eb53e4b1
kxyzqo <kxyzqo1250@gmail.com>
2024-10-09 13:46:16 +0000

    修

--- flask/main1.py
+++ flask/main1.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>外星幽靈貓貓,您好!</h1>" #修改後終端機必須關掉重啟才會更改
 
-#flask開發測試版(修改後網頁重啟就好):flask --app hello(<-module名稱) run --debug
+#flask開發測試版(修改後網頁重整就好):flask --app main1(module名稱) run --debug
