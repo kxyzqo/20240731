1c74a004b0d877bad5155e65b942ac6d46aa4fa1
kxyzqo <kxyzqo1250@gmail.com>
2024-10-09 13:25:57 +0000

    修改main.py的內容

--- flask/main.py
+++ flask/main.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/") #創造實體
 def hello_world():
-    return "<h1>Hello, World!</h1>"
-
+    return '''<h1>這是python的project</h1>
+            <p>這是在codespace環境開發的</p>
+        '''
 #gunicorn main(module名稱):app(WSGI應用程式名稱) #Ctrl+C中斷執行
