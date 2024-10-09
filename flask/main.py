2a4e27b9e0b8dc95078828d3755bf18f81f283cf
kxyzqo <kxyzqo1250@gmail.com>
2024-10-09 12:06:25 +0000

    helloworld

--- flask/main.py
+++ flask/main.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/") #創造實體
+@app.route("/") #創造實體 #gunicorn main(module名稱):app(應用程式名稱)
 def hello_world():
     return "<h1>Hello, World!</h1>"
